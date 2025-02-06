e1ad3921a7efde34b4ffcca71fb2c3e4999a19e0
Luke Olende <olendeluke2k@gmail.com>
2025-02-06 14:45:22 +0300

    divide the board into 3x3 tiles

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,32 @@
+let board;
+let playerO = "O";
+let playerX = "X";
+let currentPlayer = playerX;
+let gameOver = false;
 
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      if (r == 0 || r == 1) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 0 || c == 1) {
+        tile.classList.add("vertical-line");
+      }
+      document.getElementById("board").appendChild(tile);
+    }
+  }
+}
